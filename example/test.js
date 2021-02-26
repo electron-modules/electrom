ae1c20d995b62f2a9a4e7dcfbea67bc184a71e34
xudafeng <xudafeng@126.com>
2021-02-26 15:13:13 +0800

    feat: enhance demo

--- example/test.js
+++ example/test.js
@@ -4,48 +4,79 @@ const { BrowserWindow, app } = require('electron');
 
 // https://chromedevtools.github.io/devtools-protocol/tot/CSS/
 
-const takeCoverageDelta = async (win) => {
-  const res = await win.webContents.debugger.sendCommand('CSS.takeCoverageDelta');
-  if (res.coverage.length) {
-    for (const item of res.coverage) {
-      const { styleSheetId, startOffset, endOffset } = item;
-      const { text } = await win.webContents.debugger.sendCommand('CSS.getStyleSheetText', {
-        styleSheetId,
-      });
-      // console.log(text);
-      console.log(text.slice(startOffset, endOffset));
-    }
-  }
-};
-
-app.whenReady().then(async () => {
-  const { BrowserWindow } = require('electron')
-  const win = new BrowserWindow({
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: false,
-    }
-  });
+const attachDebuggerToWindow = async (win, options = {}) => {
+  const {
+    onCoverageChange,
+    onMetaChange,
+  } = options;
   try {
-    win.webContents.debugger.attach()
+    win.webContents.debugger.attach('1.3')
   } catch (err) {
     console.log('Debugger attach failed : ', err)
   }
 
-  win.webContents.debugger.on('detach', (event, reason) => {
-    console.log('Debugger detached due to : ', reason)
+  win.webContents.debugger.on('detach', (_, reason) => {
+    console.log('Debugger detached due to : ', reason);
   });
 
-  win.webContents.debugger.on('message', (event, method, params) => {
-    console.log(method);
+  const stylesheetMap = {};
+
+  const takeCoverageDelta = async () => {
+    const { coverage, timestamp } = await win.webContents.debugger.sendCommand('CSS.takeCoverageDelta');
+    // console.log(coverage.length, timestamp)
+    if (coverage.length) {
+      onCoverageChange({
+        coverage,
+        timestamp,
+      });
+    }
+  };
+
+  win.webContents.debugger.on('message', (_, method, params) => {
+    if (method === 'CSS.styleSheetAdded') {
+      const { header } = params;
+      // inline style
+      if (header.isInline || header.sourceURL === '' || header.sourceURL.startsWith('blob:')) {
+        if (!stylesheetMap[header.styleSheetId]) {
+          stylesheetMap[header.styleSheetId] = header;
+          onMetaChange(stylesheetMap);
+        }
+      } else if (header.sourceURL) {
+        if (!stylesheetMap[header.styleSheetId]) {
+          stylesheetMap[header.styleSheetId] = header;
+          onMetaChange(stylesheetMap);
+        }
+      }
+    }
   });
-  win.loadURL('http://localhost:8080/test/fixture');
+
+  setInterval(() => {
+    takeCoverageDelta();
+  }, 5000);
+
   await win.webContents.debugger.sendCommand('Page.enable');
   await win.webContents.debugger.sendCommand('DOM.enable');
   await win.webContents.debugger.sendCommand('CSS.enable');
   await win.webContents.debugger.sendCommand('CSS.startRuleUsageTracking');
-  
-  setInterval(() => {
-    takeCoverageDelta(win);
-  }, 5000);
-});
+};
+
+app.whenReady()
+  .then(() => {
+    const win = new BrowserWindow({
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: false,
+      }
+    });
+
+    attachDebuggerToWindow(win, {
+      onMetaChange: (data) => {
+        console.log(data);
+      },
+      onCoverageChange: (data) => {
+        console.log(data);
+      },
+    });
+
+    win.loadURL('http://localhost:8080/test/fixture');
+  });
