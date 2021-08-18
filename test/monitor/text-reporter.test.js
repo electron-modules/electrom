90ecc8abbe543f72e301214b8f57133c03e04f62
xudafeng <xudafeng@126.com>
2021-08-18 15:39:47 +0800

    feat: support text reporter

---
+++ test/monitor/text-reporter.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+const assert = require('assert');
+
+const Electrom = require('../..');
+
+describe('test/monitor/text-reporter.test.js', () => {
+  let res;
+
+  describe('pickDataFromDir()', () => {
+    it('should be ok', () => {
+      const dir = path.join(__dirname, '..', 'fixture', 'metrics');
+      res = Electrom.Monitor.TextReporter.pickDataFromDir(dir);
+      assert.equal(res.length, 2);
+    });
+  });
+
+  describe('genReporter()', () => {
+    it('should be ok', () => {
+      const dir = path.join(__dirname, '..', 'fixture', 'metrics');
+      const data = Electrom.Monitor.TextReporter.pickDataFromDir(dir);
+      res = Electrom.Monitor.TextReporter.genReporter(data);
+      console.log(res);
+    });
+  });
+});
