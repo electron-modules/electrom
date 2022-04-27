739d919311cb308cf3375c971a2d99df343891a8
zhi <yutian.yz@alibaba-inc.com>
2022-04-27 19:23:28 +0800

    chore: add column for open devtool (#23)

--- src/common/interface.ts
+++ src/common/interface.ts
@@ -1,4 +1,22 @@
 export interface ProcessInfo {
   cmd: string;
-  webContentInfo: any;
+  cpu: {
+    percentCPUUsage: number;
+    idleWakeupsPerSecond: number;
+  };
+  creationTime: number;
+  load: number;
+  memory: {
+    workingSetSize: number;
+    peakWorkingSetSize: number;
+  };
+  pid: number;
+  sandboxed: boolean;
+  type: string;
+  webContentInfo?: {
+    id: number;
+    pid: number;
+    type: string;
+    url: string;
+  };
 }
