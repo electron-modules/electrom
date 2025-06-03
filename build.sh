51232d79c6e6395e9dad71fe3d6b01dbda494280
z0gSh1u <zx.cs@qq.com>
2025-06-03 14:49:12 +0800

    chore: use `npx` instead of `npm bin` concat

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ rm -rf ./lib
 rm -rf ./dist
 
 # 2. tsc compile 
-`npm bin`/tsc
+npx tsc
 
 # 3. copy source file
-`npm bin`/copyup 'src/**/*.html' lib/
-`npm bin`/copyup 'src/**/*.less' lib/
+npx copyup 'src/**/*.html' lib/
+npx copyup 'src/**/*.less' lib/
 
 # 4. webpack build
 npm run build:web
