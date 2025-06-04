5ef9b7340e51b61f0ea7493093b97f913835630f
z0gSh1u <zx.cs@qq.com>
2025-06-04 19:44:28 +0800

    chore: force to use bin shipped with the project itself

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ rm -rf ./lib
 rm -rf ./dist
 
 # 2. tsc compile 
-npx tsc
+./node_modules/.bin/tsc
 
 # 3. copy source file
-npx copyup 'src/**/*.html' lib/
-npx copyup 'src/**/*.less' lib/
+./node_modules/.bin/copyup 'src/**/*.html' lib/
+./node_modules/.bin/copyup 'src/**/*.less' lib/
 
 # 4. webpack build
 npm run build:web
